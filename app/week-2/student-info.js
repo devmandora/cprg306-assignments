af1cf7650da670e198c1a6becb161140e4e24d24
Dev Mukesh Mandora <devmandora1234@gmail.com>
2024-09-25 10:57:08 -0600

    Week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function page(){
+    return(
+        <main>
+            <h1>Dev Mandora</h1>
+            <Link href="https://github.com/devmandora/cprg306-assignments">GitHub</Link>
+        </main>
+
+    );
+
+}
